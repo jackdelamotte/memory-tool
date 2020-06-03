7ca674787fb969de8ccc27bcd2b100f4d014debc
Jack de la Motte <jack.delamotte13@gmail.com>
2020-06-03 15:39:12 -0700

    Initial commit. UI is primitive and needs improvement, lots of room for added functionality.

---
+++ memorizer.py
@@ -0,0 +1,47 @@
+# this is a script to help a user memorize some text
+import random
+
+def main():
+
+    original_string = str(input("Enter some text you would like help memorizing: "))
+    words_list = original_string.split()
+
+    omit = 1
+
+    print("made it here")
+    prompt_rec(omit, words_list, original_string)
+
+
+def prompt_rec(n, lst, strng):
+
+    # base case
+    if n == len(lst):
+        return 0
+
+    
+    for i in range(n):
+
+        index = random.randint(0, len(lst) - 1)
+
+        lst[index] = '_' * len(lst[index])
+
+ 
+    attempt = ''
+
+    while(attempt != strng):
+
+        # to clear the console
+        count = 0
+        while (count < 50):
+            print()
+            count += 1
+
+        print(" ".join(lst))
+
+        attempt = input("using the partial text above, type the original text, then press enter: ")
+        print()
+
+    return prompt_rec(n+1, lst, strng)
+
+if __name__ == "__main__":
+    main()
